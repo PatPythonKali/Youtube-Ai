10135b0371e58a68ac524c1d66fafc47bda81960
ubuntu-server <patpythonkali@gmail.com>
2021-08-08 17:48:26 +0800

    FRANCIS LEO MARCOS UPDATES Channel

---
+++ trash.py
@@ -0,0 +1,20 @@
+links = [1, 2, 3, 4, 5, 6, 7, 8]
+
+for num in links:
+    if num % 2 == 0:
+        continue
+    else:
+        print(num, end=" ")
+
+# phone_number = "123-456-7890"
+#
+# for i in phone_number:
+#     if i == "-":
+#         continue
+#     print(i,end="")
+
+# for i in range(1,21):
+#     if i == 13:
+#         pass
+#     else:
+#         print(i)
