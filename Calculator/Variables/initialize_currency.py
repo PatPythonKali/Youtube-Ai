df48e2bbfffb6d27250f3cb51581aca11692ab85
ubuntu-server <patpythonkali@gmail.com>
2021-08-09 13:19:28 +0800

    Currency Initialize

---
+++ Calculator/Variables/initialize_currency.py
@@ -0,0 +1,10 @@
+gross_profit_total = 0
+
+hk_superchatters = 0
+cad_superchatters = 0
+usd_superchatters = 0
+php_superchatters = 0
+gbp_superchatters = 0
+aud_superchatters = 0
+qar_superchatters = 0
+sgd_superchatters = 0
---
+++ Calculator/main.py
@@ -0,0 +1,8 @@
+# Import External Library
+import json
+from forex_python.converter import CurrencyRates
+
+# Import Internal Library
+from Calculator.Variables.initialize_currency import *
+
+print(gross_profit_total)
