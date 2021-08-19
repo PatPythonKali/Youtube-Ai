c1c5628506ddd4273b134daa5658519cba767ec9
ubuntu <patpyhtonkali@gmail.com>
2021-08-20 04:34:50 +0800

    Date time filter update

--- Tools/json_scraper.py
+++ Tools/json_scraper.py
@@ -1,18 +1,19 @@
 # External Libraries
+import time
+start = time.time()
 import pytchat
 import json
-
+import os
 
 # Internal Libraries
-from URL.francis_leo_marcos_updates import *
+from URL.flm_mayaman_challenge_20 import *
 
 # Target Input
-video_id = francis_leo_marcos_updates_url
+video_id = flm_mayaman_challenge_20_url
 
 # Create, Save and Overwrite Text File
-with open('/home/ubuntu/PycharmProjects/Youtube-Ai/Harvest/FLM/' +
+with open(os.path.abspath(f"../Harvest/FLM/") +
           channel_name.replace(" ", "") + ".JSON", 'w', encoding='utf-8') as write_file:
-
 
     # Loop between Video ID's
     for v_id in video_id:
@@ -28,16 +29,8 @@ with open('/home/ubuntu/PycharmProjects/Youtube-Ai/Harvest/FLM/' +
                 obj = c.json()
                 obj2 = json.loads(obj)
 
-
-                # Objects
-                yt_author_name = obj2['author']['name']
-                yt_author_id = obj2['author']['channelId']
-                yt_message = obj2['message']
-                yt_datetime = obj2['datetime']
-                yt_elapsedTime = obj2['elapsedTime']
-
-                print(F"{yt_datetime} | {yt_elapsedTime}")
-                print(F"{yt_author_name}: {yt_message} \n")
-
                 # Output to File Regular
                 json.dump(obj2, write_file, indent=4)
+
+end = time.time()
+print(f"Total Execution Time: {round(end - start, 2)}")
