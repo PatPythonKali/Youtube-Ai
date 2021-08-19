d0d533456db8916e79c6e8cba4763dae0b25bda1
ubuntu <patpyhtonkali@gmail.com>
2021-08-20 04:05:42 +0800

    Date time filter update

--- URL/flm_mayaman_challenge_20.py
+++ URL/flm_mayaman_challenge_20.py
@@ -1,7 +1,6 @@
 channel_name = " FLM MAYAMAN CHALLENGE 2.0"
 flm_mayaman_challenge_20_url=[
-    "nEu-BpCztRw",
-]
+    "nEu-BpCztRw",]
 
 # Remove Duplicates (Ensures that no duplicates will be on the list)
 flm_mayaman_challenge_20_url = list(dict.fromkeys(flm_mayaman_challenge_20_url))
