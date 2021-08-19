cf9d355978b953201687ebcfd785fcae92c6b21f
ubuntu <patpyhtonkali@gmail.com>
2021-08-20 05:08:40 +0800

    Updates

--- Calculator/Variables/load_json.py
+++ Calculator/Variables/load_json.py
@@ -2,8 +2,8 @@ import json
 
 def load_json():
 
-    # with open('/home/ubuntu/Documents/JSON/FLM JSON PROCESSED/FLMMAYAMANCHALLENGE20.JSON') as json_file:
-    with open('/home/ubuntu/Documents/JSON/FLMJSONAug1-19PROCESSED/FLM-PROCESSED-AUG-1-19-MASTER.JSON') as json_file:
+    with open('/home/ubuntu/PycharmProjects/Youtube-Ai/Harvest/FLMFLMMAYAMANCHALLENGE2.0.JSON') as json_file:
+    # with open('/home/ubuntu/Documents/JSON/FLMJSONAug1-19PROCESSED/FLM-PROCESSED-AUG-1-19-MASTER.JSON') as json_file:
         obj = json_file.read()
         obj2 = json.loads(obj)
         return obj2
