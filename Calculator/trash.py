d8c4663cdf2a4c4361e85384c1e65e1743041124
ubuntu-server <patpythonkali@gmail.com>
2021-08-29 23:59:52 +0800

    Updates

--- Calculator/trash.py
+++ Calculator/trash.py
@@ -1,9 +1,9 @@
 import time
+
 start = time.time()
 
 from Variables.initialize_currency import *
 from Variables.load_json import load_json
-
 
 for x in load_json():
 
@@ -16,6 +16,7 @@ for x in load_json():
     message = x['message']
     date_time = x['datetime']
     id = x['id']
+    name = x['author']['name']
 
     chat_sponsor = x['author']['isChatSponsor']
     chat_moderator = x['author']['isChatModerator']
@@ -25,166 +26,172 @@ for x in load_json():
 
     if amount != 0:
 
+
         if currency == "₱":
             x = round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "USD":
             x = 50.45 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "CAD":
             x = 40.26 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "¥":
             x = 0.46 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "HKD":
-            x = 6.48  * round(amount)
+            x = 6.48 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "EUR":
             x = 59.10 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "GBP":
-            x = 69.70  * round(amount)
+            x = 69.70 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "AUD":
             x = 36.97 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "SGD":
             x = 37.08 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "CHF":
             x = 54.61 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "QAR\u00a0":
             x = 13.86 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "TWD":
             x = 1.81 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "₪":
             x = 15.64 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "AED\u00a0":
             x = 13.73 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "KRW":
             x = .044 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "SEK":
             x = 5.78 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "NOK":
             x = 5.65 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "NZD":
             x = 35.31 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "RUB":
             x = 0.68 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "SAR\u00a0":
             x = 13.45 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "INR":
             x = 0.68 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "MAD\u00a0":
             x = 5.62 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
 
         if currency == "DKK\u00a0":
             x = 7.95 * round(amount)
             gross_profit_total += x
             super_chat_counter += 1
 
-            print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+            # print(f"{x} | {round(gross_profit_total, 2)} | {super_chat_counter}")
+
+
+        print(f"{round(gross_profit_total, 2)} | {super_chat_counter}")
+
+
 
 end = time.time()
 result = round(end - start, 2)
---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+window = Tk()
+window.title("Youtube AI")
+window.geometry("1024x768")
+window.resizable(False, False)
+
+
+window.mainloop()
